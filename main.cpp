fa69ccad525febd44fe55a9ff5d8999e203e817d
Danizet <lproanoc1@est.ups.edu.ec>
2022-05-31 10:34:17 -0500

    Creada la clase Persona

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
+#include <persona.h>
+
 using namespace std;
 
 int main()
 {
     cout << "Mi primer proyecto en Git" << endl;
+    Persona p;
+    p.nombre = "Daniel";
+    p.edad = 21;
+    cout << p.nombre << endl;
+    cout << p.edad << endl;
     return 0;
 }
---
+++ persona.h
@@ -0,0 +1,16 @@
+#ifndef PERSONA_H
+#define PERSONA_H
+
+#include <iostream>
+
+using namespace std;
+
+class Persona
+{
+public:
+    Persona();
+    int edad;
+    string nombre;
+};
+
+#endif // PERSONA_H
